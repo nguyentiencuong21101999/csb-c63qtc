db35cf523a427d5abda5f33f89c28b37e9f4971c
Code Sandbox <hello@codesandbox.io>
2022-04-08 04:06:24 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b6ca09ae935282359e3da9e07a111729.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c168fc09b92b3fbf9477b467e1593f",
+    "url": "https://nguyentiencuong21101999.github.io/csb-c63qtc/index.html"
+  },
+  {
+    "revision": "3bcf81e23261f3cec28e",
+    "url": "https://nguyentiencuong21101999.github.io/csb-c63qtc/static/css/2.aab1eca5.chunk.css"
+  },
+  {
+    "revision": "39555ee563b04a81d25c",
+    "url": "https://nguyentiencuong21101999.github.io/csb-c63qtc/static/css/main.03c17d53.chunk.css"
+  },
+  {
+    "revision": "3bcf81e23261f3cec28e",
+    "url": "https://nguyentiencuong21101999.github.io/csb-c63qtc/static/js/2.a750bf13.chunk.js"
+  },
+  {
+    "revision": "39555ee563b04a81d25c",
+    "url": "https://nguyentiencuong21101999.github.io/csb-c63qtc/static/js/main.393eaf1f.chunk.js"
+  },
+  {
+    "revision": "913206fe97fe67e0ae6c",
+    "url": "https://nguyentiencuong21101999.github.io/csb-c63qtc/static/js/runtime~main.9412fdb8.js"
+  }
+]);
